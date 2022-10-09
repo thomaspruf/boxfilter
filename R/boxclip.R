352f8196616b290352ad96300e782d1cacda3dbc
thomaspruf <59416686+thomaspruf@users.noreply.github.com>
2022-10-09 16:21:17 +0200

    Add files via upload

--- R/boxclip.R
+++ R/boxclip.R
@@ -29,7 +29,8 @@ boxclip <- function(x,y,clipit=NULL,QI=NULL,width=NULL, height=NULL, miny=10, pl
          lo<-this-height
          test<-ndat$y[sect]
          nei<-sum(test>=lo & test<=hi,na.rm=T)
-         ndat$neighbors[i]<-nei/(2*width+1)
+         all=length(ndat$y[sect])
+         ndat$neighbors[i]<-nei/all#(2*width+1)
       }
   }
 
